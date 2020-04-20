58211a66e74125bac841a195f9fd7fd9f684ca76
yzi2 <yzi2@central.uh.edu>
2020-04-20 00:11:55 -0500

    update

--- test.py
+++ test.py
@@ -1,32 +1,59 @@
-"""test.py: Main code to train parking slots monitor"""
+# coding: utf-8
+"""test.py: code to test parking slots monitor"""
 
-# Example Usage: ./cv_hw1 -i image -k clusters -m grey
-# Example Usage: python cv_hw1 -i image -k clusters -m rgb
+
 
 
 __author__ = "Yuan Zi"
 __email__ = "yzi2@central.uh.edu"
 __version__ = "1.0.0"
 
-import cv2
-​
-face = cv2.CascadeClassifier("./data/cascade.xml")  # 人脸识别
-cap = cv2.VideoCapture(2)
-​
-while True:
-    ret, img = cap.read()
-    roi_color = 0
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # 转换为灰度图片，haar特征都是在灰度上检测
-    faces = face.detectMultiScale(gray, 1.3, 5)  # 参数1：灰度图片数据   2:缩放比例 3：人脸大小不能小于5个像素
-​
-# 获取宽高信息，个人脸画方框
-for (x, y, w, h) in faces:
-    # img：要画的图片，(x,y)：起始坐标 ，(x+w,y+h)：宽高，颜色，线条宽度
-    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+def main():
+    """ The main funtion that parses input arguments, calls the test image and models"""
 
-cv2.imshow('10', img)
-if cv2.waitKey(5) & 0xff == ord("q"):
-    break
-​
-cap.release()
-cv2.destroyAllWindows()
+    #Parse input arguments
+    from argparse import ArgumentParser
+    from pathlib import Path
+    import sys
+
+    parser = ArgumentParser()
+
+    parser.add_argument("-model", type=Path, help="specify the path of the input image")
+    parser.add_argument("-image", type=Path, help="Specify the the path of the output files and images")
+
+    args = parser.parse_args()
+
+    #Load image
+    if args.path_input is None:
+        print("Please specify the path of the input dataset")
+        print("use the -h option to see usage information")
+        sys.exit(2)
+    else:
+        model_path = args.model
+
+
+    if args.path_output is None:
+        print("Please specify the path of the output dataset")
+        print("use the -h option to see usage information")
+    else:
+        image_path = args.image
+
+
+    import cv2
+
+    car_cascade = cv2.CascadeClassifier(str(model_path))
+    print('after loading model')
+    img = cv2.imread(str(image_path))
+    print('after loading image')
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    print("after coverting to gray")
+    cars = car_cascade.detectMultiScale(gray, 1.3, 5)
+    print('after detection')
+    i = 0
+    for (x, y, w, h) in cars:
+
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        print("bounding box",i)
+        i = i + 1
+    cv2.imwrite('test.jpg', img)
+
