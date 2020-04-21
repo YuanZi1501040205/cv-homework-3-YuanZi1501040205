23f41621b7779e2e42e85ee170b4b21af8682e2a
yuanzi1501040205 <yzi2@central.uh.edu>
2020-04-21 14:26:38 -0500

    update

--- Functions.py
+++ Functions.py
@@ -8,11 +8,55 @@ __author__ = "Yuan Zi"
 __email__ = "yzi2@central.uh.edu"
 __version__ = "1.0.0"
 
-def read_xml(path_xml, space_id):
-
-       import xml.etree.ElementTree as ET
-
-       return(image, center, theta, width, height)
+# def analysis(path_input, numSamples):
+#        import cv2
+#        import xml.etree.ElementTree as ET
+#        import os
+#        import glob
+#
+#        path_data1 = str(path_input) + '/parking1a/'
+#        path_data2 = str(path_input) + '/parking1b/'
+#        path_data3 = str(path_input) + '/parking2/'
+#        path_data = []
+#        path_data.append(path_data1)
+#        path_data.append(path_data2)
+#        path_data.append(path_data3)
+#        gt_boxes = {}
+#        for root_path in path_data:
+#               cloudy_folders_under_root = os.listdir(root_path + 'cloudy')
+#               rainy_folders_under_root = os.listdir(root_path + 'rainy')
+#               # Traverse all folders under the root folder, change the root1_folder to path_data1,2,3 respectively and rerun this cell
+#               for folders in cloudy_folders_under_root:
+#                      paths_image_cloudy = glob.glob(
+#                             os.path.join(root_path + 'cloudy/' + folders, '*.jpg'))  # get all images in this path
+#                      # Traverse all images; selected images based on the requirment number of images for testing. 25 rainy, 25 test
+#                      for path in paths_image_cloudy:
+#                             image = cv2.imread(path)
+#                             path_xml = os.path.splitext(path)[0] + '.xml'  # read xml file associated with image
+#                             tree = ET.parse(path_xml)
+#                             spaces = tree.findall("space")  # read all parking slots' bounding box's informations
+#                             bbx = []
+#                             for space in spaces:
+#                                    rotatedRect = space.find('rotatedRect')
+#                                    id = space.attrib['id']
+#                                    bbx.append([space.])
+#                             gt_boxes.update(path:)
+#               for folders in rainy_folders_under_root:
+#                      rainy_image_cloudy = glob.glob(
+#                             os.path.join(root_path + 'rainy/' + folders, '*.jpg'))  # get all images in this path
+#        # GT Boxes
+#        gt_boxes = {"img_00285.png": [[480, 457, 515, 529], [637, 435, 676, 536]]}
+#        # Pred Boxes
+#        pred_boxs = {"img_00285.png": {
+#               "boxes": [[330, 463, 387, 505], [356, 456, 391, 521], [420, 433, 451, 498], [328, 465, 403, 540],
+#                         [480, 477, 508, 522], [357, 460, 417, 537], [344, 459, 389, 493], [485, 459, 503, 511],
+#                         [336, 463, 362, 496], [468, 435, 520, 521], [357, 458, 382, 485], [649, 479, 670, 531],
+#                         [484, 455, 514, 519], [641, 439, 670, 532]],
+#               "scores": [0.0739, 0.0843, 0.091, 0.1008, 0.1012, 0.1058, 0.1243, 0.1266, 0.1342, 0.1618, 0.2452, 0.8505,
+#                          0.9113, 0.972]}}
+#        import xml.etree.ElementTree as ET
+#
+#        return(image, center, theta, width, height)
 
 def extract_roi(image, center, theta, width, height):
        '''
--- test.py
+++ test.py
@@ -20,6 +20,18 @@ def main():
 
     parser.add_argument("-model", type=Path, help="specify the path of the input image")
     parser.add_argument("-image", type=Path, help="Specify the the path of the output files and images")
+    parser.add_argument("-scale_factor", "--scale_factor", dest="scale_factor",
+                        help="Specify how much the image size is reduced at each image scale.")
+    parser.add_argument("-min_neighbors", "--min_neighbors", dest="min_neighbors",
+                        help="Specify Parameter specifying how many neighbors each candidate rectangle should have to retain it.")
+    parser.add_argument("-min_size_w", "--min_size_w", dest="min_size_w",
+                        help="Specify Minimum possible object size. Objects smaller than that are ignored")
+    parser.add_argument("-min_size_h", "--min_size_h", dest="min_size_h",
+                        help="Specify Minimum possible object size. Objects smaller than that are ignored")
+    parser.add_argument("-maxSize_w", "--maxSize_w", dest="maxSize_w",
+                        help="Specify Maximum possible object size. Objects larger than that are ignored.")
+    parser.add_argument("-maxSize_h", "--maxSize_h", dest="maxSize_h",
+                        help="Specify Maximum possible object size. Objects larger than that are ignored.")
 
     args = parser.parse_args()
 
@@ -38,6 +50,35 @@ def main():
     else:
         image_path = args.image
 
+    if args.scale_factor is None:
+        print("use the -h option to see usage information")
+    else:
+        scale_factor = args.scale_factor
+
+    if args.min_neighbors is None:
+        print("use the -h option to see usage information")
+    else:
+        min_neighbors = args.min_neighbors
+
+    if args.min_size_w is None:
+        print("use the -h option to see usage information")
+    else:
+        min_size_w = args.min_size_w
+
+    if args.min_size_h is None:
+        print("use the -h option to see usage information")
+    else:
+        min_size_h = args.min_size_h
+
+    if args.maxSize_w is None:
+        print("use the -h option to see usage information")
+    else:
+        maxSize_w = args.maxSize_w
+
+    if args.maxSize_h is None:
+        print("use the -h option to see usage information")
+    else:
+        maxSize_h = args.maxSize_h
 
     import cv2
 
@@ -47,7 +88,7 @@ def main():
     print('after loading image')
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     print("after coverting to gray")
-    cars = car_cascade.detectMultiScale(gray, 1.3, 5)
+    cars = car_cascade.detectMultiScale(gray, scaleFactor=float(scale_factor), minNeighbors=int(min_neighbors), minSize=(int(min_size_w), int(min_size_h)), maxSize=(int(maxSize_w),int(maxSize_h)))
     print('after detection')
     i = 0
     for (x, y, w, h) in cars:
@@ -55,8 +96,7 @@ def main():
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
         print("bounding box",i)
         i = i + 1
-    cv2.imwrite('test.jpg', img)
-
+    cv2.imwrite(str(image_path).split('/')[-1].split('.')[0] +'scale'+scale_factor+'minneighbors'+min_neighbors+'minh'+min_size_h+'minw'+min_size_w+'maxh'+maxSize_h+'maxw'+maxSize_w+ '_test.jpg', img)
 
 if __name__ == "__main__":
     main()
